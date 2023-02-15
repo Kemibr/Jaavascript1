2e16f9e5b21897e276462f73057ba39d779f0deb
Kemily Barbosa <125513264+Kemibr@users.noreply.github.com>
2023-02-15 19:38:50 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+let numero1 = 10
+let numero2 = 20
+let numero3 = 10 + 20
+let numero4 = 50
+
+
+
+let soma = numero1 + numero2
+let sub = numero1 - numero2
+let div = numero1 / numero2
+let mult = numero1 * numero2
+let numero5 = numero4
+
+console.log(soma)
+console.log(sub)
+console.log(div)
+console.log(mult)
+console.log(numero3)
+console.log(numero5)
